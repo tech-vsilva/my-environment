19052012a527fbb1a479beb95b01b92f3980f987
Valter Silva <valter.silva@orderbird.com>
2017-09-21 21:44:28 +0200

    create function to simplify update command

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,20 @@
 
 set -x
 
+update(){
+  sudo apt-get update
+}
+
 # https://askubuntu.com/a/98467
 is_installed() {
-<<<<<<< HEAD
-    # dpkg -l "${1}" | return $?
-=======
-    dpkg -l "${1}" | grep -q ^ii && return 1
->>>>>>> parent of ef09d8e... invert return status
-    return 0
+    dpkg -l "${1}" | grep -q ^ii && return 0
+    return 1
 }
 
 install_atom(){
   if ! is_installed atom; then
       sudo add-apt-repository -y ppa:webupd8team/atom \
-      && sudo apt-get update && sudo apt-get install -y atom
+      && update && sudo apt-get install -y atom
   fi
 }
 
@@ -23,11 +23,11 @@ install_google_chrome(){
   if ! is_installed google-chrome-stable; then
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - \
     && sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' \
-    && sudo apt-get update \
+    && update \
     && sudo apt-get install google-chrome-stable
   fi
 }
 
-sudo apt-get update
+update
 install_atom
 install_google_chrome
