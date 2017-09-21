47ebca8cc38dac91434e27c249f61a25d4523aa4
Valter Silva <valter.silva@orderbird.com>
2017-09-21 21:01:03 +0200

    update and install atom

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@
 set -x
 
 install_atom(){
-  sudo add-apt-repository -y ppa:webupd8team/atom
+  sudo add-apt-repository -y ppa:webupd8team/atom \
+  && sudo apt-get update && sudo apt-get install -y atom
 }
 
 sudo apt-get update \
