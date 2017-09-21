92d3665a7cff982d8bd78eabf412a7c82a5e30ad
Valter Silva <valter.silva@orderbird.com>
2017-09-21 21:47:04 +0200

    create function to simplify install operation

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,10 @@ update(){
   sudo apt-get update
 }
 
+install(){
+  sudo apt-get -qqy install "${1}" "$@"
+}
+
 # https://askubuntu.com/a/98467
 is_installed() {
     dpkg -l "${1}" | grep -q ^ii && return 0
@@ -15,7 +19,7 @@ is_installed() {
 install_atom(){
   if ! is_installed atom; then
       sudo add-apt-repository -y ppa:webupd8team/atom \
-      && update && sudo apt-get install -y atom
+      && update && install atom
   fi
 }
 
@@ -23,8 +27,7 @@ install_google_chrome(){
   if ! is_installed google-chrome-stable; then
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - \
     && sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' \
-    && update \
-    && sudo apt-get install google-chrome-stable
+    && update && install google-chrome-stable
   fi
 }
 
