d02ba1a872c0ddc94d2544abcde08e8ea09c8069
Valter Silva <valter.silva@orderbird.com>
2017-09-21 22:42:04 +0200

    create report about the status of the bootstrap operation

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,33 @@
 
 set -x
 
+REPORT="${HOME}/bootstrap.txt"
+
+rm -f "${REPORT}"
+
 update(){
   sudo apt-get update
 }
 
-install(){
-  sudo apt-get -qqy install "${1}"
+report(){
+  echo "${1}" | tee -a "${REPORT}"
 }
 
-# https://askubuntu.com/a/98467
+install(){
+  report "installing ${1}"
+  sudo apt-get -qqy install "${1}"
+  if is_installed "${1}"; then
+    report "${1} installed successfully!"
+  fi
+}
+
+# verify if a package is installed
+# return 0 if true, 1 if false
+# Also informs about it in a report
 is_installed() {
-    dpkg -l "${1}" | grep -q ^ii && return 0
+    dpkg -l "${1}" | grep -q ^ii \
+    && report "${1} already installed, skipping." && return 0
+    # return
     return 1
 }
 
@@ -26,11 +42,26 @@ install_atom(){
 install_google_chrome(){
   if ! is_installed google-chrome-stable; then
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - \
-    && sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' \
+    && echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list \
     && update && install google-chrome-stable
   fi
+}
+
+install_spotify(){
+  if ! is_installed spotify-client; then
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 \
+    && echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list \
+    && update && install spotify-client
+  fi
+}
+
+show_report(){
+  cat "${REPORT}"
 }
 
 update
 install_atom
 install_google_chrome
+install_spotify
+
+show_report
